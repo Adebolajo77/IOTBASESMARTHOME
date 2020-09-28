6c553b9e1173995437c3c8e8898b8a14c6b39f81
Adebolajo77 <59399318+Adebolajo77@users.noreply.github.com>
2020-09-28 16:16:13 +0100

    Update dataBaseMC.php

--- nxt/dataBaseMC.php
+++ nxt/dataBaseMC.php
@@ -8,13 +8,13 @@ class Database{
     }
     
     function connect(){
-        $this->link = mysqli_connect('localhost','root','') or die('Cannot connect to the DB');
-        mysqli_select_db($this->link,'datamonitoringcontrol') or die('Cannot select the DB');
+        $this->link = mysqli_connect('localhost','root','') or die('Cannot connect to the DB'); //replaced your data base host,user and password
+        mysqli_select_db($this->link,'datamonitoringcontrol') or die('Cannot select the DB');   // database name datamonitoringcontrol can also be change
     }
     
     function saveDB($temp,$hum){
 
-        $query = "   insert into datacontrol set temperature='".$temp."',humidity='".$hum."'     ";
+        $query = "   insert into datacontrol set temperature='".$temp."',humidity='".$hum."'     ";  //table name with for column ID(INT,KEY, AUTO INCREMENT),temperature(INT),humidity(INT) and date (timestamp)
         $result = mysqli_query($this->link,$query) or die('Errant query:  '.$query);
         $this->link->close();
     }
